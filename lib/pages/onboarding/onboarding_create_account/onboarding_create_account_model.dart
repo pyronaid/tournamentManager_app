c05221b5f88e0102ffacfdf605b08952b13167c6
pyronaid <64960586+pyronaid@users.noreply.github.com>
2024-07-24 11:26:32 +0200

    Update onboarding_create_account_model.dart

--- lib/pages/onboarding/onboarding_create_account/onboarding_create_account_model.dart
+++ lib/pages/onboarding/onboarding_create_account/onboarding_create_account_model.dart
@@ -1,42 +1,84 @@
+import '/auth/firebase_auth/auth_util.dart';
+import '/backend/backend.dart';
 import '/components/custom_appbar_widget.dart';
-import '/flutter_flow/flutter_flow_animations.dart';
-import '/flutter_flow/flutter_flow_theme.dart';
-import '/flutter_flow/flutter_flow_util.dart';
-import '/flutter_flow/flutter_flow_widgets.dart';
-import 'dart:math';
-import 'onboarding_slideshow_widget.dart' show OnboardingSlideshowWidget;
-import 'package:smooth_page_indicator/smooth_page_indicator.dart'
-    as smooth_page_indicator;
+import 'onboarding_create_account_widget.dart' show OnboardingCreateAccountWidget;
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_animate/flutter_animate.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
-class OnboardingSlideshowModel extends CustomFlowModel<OnboardingSlideshowWidget> {
+class OnboardingCreateAccountModel extends FlutterFlowModel<OnboardingCreateAccountWidget> {
   ///  State fields for stateful widgets in this page.
 
   final unfocusNode = FocusNode();
+  final formKey = GlobalKey<FormState>();
   // Model for customAppbar component.
   late CustomAppbarModel customAppbarModel;
-  // State field(s) for PageView widget.
-  PageController? pageViewController;
+  // State field(s) for fullName widget.
+  FocusNode? fullNameFocusNode;
+  TextEditingController? fullNameTextController;
+  String? Function(BuildContext, String?)? fullNameTextControllerValidator;
+  String? _fullNameTextControllerValidator(BuildContext context, String? val) {
+    if (val == null || val.isEmpty) {
+      return 'Il nome è un parametro obbligatorio';
+    }
 
-  int get pageViewCurrentIndex => pageViewController != null &&
-          pageViewController!.hasClients &&
-          pageViewController!.page != null
-      ? pageViewController!.page!.round()
-      : 0;
+    return null;
+  }
+
+  // State field(s) for emailAddress widget.
+  FocusNode? emailAddressFocusNode;
+  TextEditingController? emailAddressTextController;
+  String? Function(BuildContext, String?)? emailAddressTextControllerValidator;
+  String? _emailAddressTextControllerValidator(BuildContext context, String? val) {
+    if (val == null || val.isEmpty) {
+      return 'La mail è un parametro obbligatorio';
+    }
+
+    if (!RegExp(kTextValidatorEmailRegex).hasMatch(val)) {
+      return 'La mail inserita non è un indirizzo valido';
+    }
+    return null;
+  }
+
+  // State field(s) for password widget.
+  FocusNode? passwordFocusNode;
+  TextEditingController? passwordTextController;
+  late bool passwordVisibility;
+  String? Function(BuildContext, String?)? passwordTextControllerValidator;
+  String? _passwordTextControllerValidator(BuildContext context, String? val) {
+    if (val == null || val.isEmpty) {
+      return 'La password è un parametro obbligatorio';
+    }
+
+    if (!RegExp(kTextValidatorPasswordRegex).hasMatch(val)) {
+      return 'La password deve contenere almeno 8 caratteri, una maiuscola, una minuscola, un numero e un carattere speciale';
+    }
+
+    return null;
+  }
 
   @override
   void initState(BuildContext context) {
     customAppbarModel = createModel(context, () => CustomAppbarModel());
+    fullNameTextControllerValidator = _fullNameTextControllerValidator;
+    emailAddressTextControllerValidator = _emailAddressTextControllerValidator;
+    passwordVisibility = false;
+    passwordTextControllerValidator = _passwordTextControllerValidator;
   }
 
   @override
   void dispose() {
     unfocusNode.dispose();
     customAppbarModel.dispose();
+    fullNameFocusNode?.dispose();
+    fullNameTextController?.dispose();
+
+    emailAddressFocusNode?.dispose();
+    emailAddressTextController?.dispose();
+
+    passwordFocusNode?.dispose();
+    passwordTextController?.dispose();
   }
 }
