784df327f57213b7bbc05ed3b7d0d64389e4ccdf
pyronaid <64960586+pyronaid@users.noreply.github.com>
2024-07-26 17:33:54 +0200

    Create sign_in_model.dart

---
+++ lib/pages/onboarding/sign_in/sign_in_model.dart
@@ -0,0 +1,60 @@
+
+
+class SignInModel extends FlutterFlowModel<SignInWidget> {
+  ///  State fields for stateful widgets in this page.
+
+  
+  final unfocusNode = FocusNode();
+  final formKey = GlobalKey<FormState>();
+  // Model for customAppbar component.
+  late CustomAppbarModel customAppbarModel;
+  // State field(s) for emailAddress widget.
+  FocusNode? emailAddressFocusNode;
+  TextEditingController? emailAddressTextController;
+  String? Function(BuildContext, String?)? emailAddressTextControllerValidator;
+  String? _emailAddressTextControllerValidator(BuildContext context, String? val) {
+    if (val == null || val.isEmpty) {
+      return 'La mail è un parametro obbligatorio';
+    }
+    if (!RegExp(kTextValidatorEmailRegex).hasMatch(val)) {
+      return 'La mail inserita non è un indirizzo valido';
+    }
+    return null;
+  }
+
+
+  // State field(s) for password widget.
+  FocusNode? passwordFocusNode;
+  TextEditingController? passwordTextController;
+  late bool passwordVisibility;
+  String? Function(BuildContext, String?)? passwordTextControllerValidator;
+  String? _passwordTextControllerValidator(BuildContext context, String? val) {
+    if (val == null || val.isEmpty) {
+      return 'La password è un parametro obbligatorio';
+    }
+    if (!RegExp(kTextValidatorPasswordRegex).hasMatch(val)) {
+      return 'La password deve contenere almeno 8 caratteri, una maiuscola, una minuscola, un numero e un carattere speciale';
+    }
+
+    return null;
+  }
+
+  @override
+  void initState(BuildContext context) {
+    customAppbarModel = createModel(context, () => CustomAppbarModel());
+    emailAddressTextControllerValidator = _emailAddressTextControllerValidator;
+    passwordVisibility = false;
+    passwordTextControllerValidator = _passwordTextControllerValidator;
+  }
+
+  @override
+  void dispose() {
+    unfocusNode.dispose();
+    customAppbarModel.dispose();
+    emailAddressFocusNode?.dispose();
+    emailAddressTextController?.dispose();
+
+    passwordFocusNode?.dispose();
+    passwordTextController?.dispose();
+  }
+}
