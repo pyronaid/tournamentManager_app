f376da271e2475a2bcfad5f57ecd3ec79850196c
pyronaid <64960586+pyronaid@users.noreply.github.com>
2024-07-29 12:55:39 +0200

    Create edit_profile_model.dart

---
+++ lib/pages/profile/edit_profile/edit_profile_model.dart
@@ -0,0 +1,45 @@
+class EditProfileModel extends FlutterFlowModel<EditProfileWidget> {
+  ///  Local state fields for this page.
+
+  bool unsavedChanges = false;
+
+  ///  State fields for stateful widgets in this page.
+
+  final unfocusNode = FocusNode();
+  // Model for customAppbar component.
+  late CustomAppbarModel customAppbarModel;
+  // State field(s) for fullName widget.
+  FocusNode? fullNameFocusNode;
+  TextEditingController? fullNameTextController;
+  String? Function(BuildContext, String?)? fullNameTextControllerValidator;
+  // Model for titleWithSubtitle component.
+  late TitleWithSubtitleModel titleWithSubtitleModel1;
+  // Model for titleWithSubtitle component.
+  late TitleWithSubtitleModel titleWithSubtitleModel2;
+  // State field(s) for emailAddress widget.
+  FocusNode? emailAddressFocusNode;
+  TextEditingController? emailAddressTextController;
+  String? Function(BuildContext, String?)? emailAddressTextControllerValidator;
+
+  @override
+  void initState(BuildContext context) {
+    customAppbarModel = createModel(context, () => CustomAppbarModel());
+    titleWithSubtitleModel1 =
+        createModel(context, () => TitleWithSubtitleModel());
+    titleWithSubtitleModel2 =
+        createModel(context, () => TitleWithSubtitleModel());
+  }
+
+  @override
+  void dispose() {
+    unfocusNode.dispose();
+    customAppbarModel.dispose();
+    fullNameFocusNode?.dispose();
+    fullNameTextController?.dispose();
+
+    titleWithSubtitleModel1.dispose();
+    titleWithSubtitleModel2.dispose();
+    emailAddressFocusNode?.dispose();
+    emailAddressTextController?.dispose();
+  }
+}
