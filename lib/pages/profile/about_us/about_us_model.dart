ffec4272c0755129c73bba021367132669d684f0
pyronaid <64960586+pyronaid@users.noreply.github.com>
2024-07-29 11:23:23 +0200

    Create about_us_model.dart

---
+++ lib/pages/profile/about_us/about_us_model.dart
@@ -0,0 +1,18 @@
+class AboutUsModel extends FlutterFlowModel<AboutUsWidget> {
+  ///  State fields for stateful widgets in this page.
+
+  final unfocusNode = FocusNode();
+  // Model for customAppbar component.
+  late CustomAppbarModel customAppbarModel;
+
+  @override
+  void initState(BuildContext context) {
+    customAppbarModel = createModel(context, () => CustomAppbarModel());
+  }
+
+  @override
+  void dispose() {
+    unfocusNode.dispose();
+    customAppbarModel.dispose();
+  }
+}
