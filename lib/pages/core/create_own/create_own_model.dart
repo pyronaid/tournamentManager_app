1ffb486eb8e365001357fa40296771773ff8862d
pyronaid <64960586+pyronaid@users.noreply.github.com>
2024-07-29 17:59:24 +0200

    Update create_own_model.dart

--- lib/pages/core/create_own/create_own_model.dart
+++ lib/pages/core/create_own/create_own_model.dart
@@ -1,1 +1,18 @@
+class CreateOwnModel extends FlutterFlowModel<CreateOwnWidget> {
+  ///  State fields for stateful widgets in this page.
 
+  final unfocusNode = FocusNode();
+  // Model for customAppbar component.
+  late CustomAppbarModel customAppbarModel;
+
+  @override
+  void initState(BuildContext context) {
+    customAppbarModel = createModel(context, () => CustomAppbarModel());
+  }
+
+  @override
+  void dispose() {
+    unfocusNode.dispose();
+    customAppbarModel.dispose();
+  }
+}
