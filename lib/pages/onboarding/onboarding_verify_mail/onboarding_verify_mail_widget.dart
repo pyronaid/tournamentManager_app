47742604b832eceb5e8ef185d041ae3f59d52c3e
pyronaid <64960586+pyronaid@users.noreply.github.com>
2024-08-02 12:48:34 +0200

    Update onboarding_verify_mail_widget.dart

--- lib/pages/onboarding/onboarding_verify_mail/onboarding_verify_mail_widget.dart
+++ lib/pages/onboarding/onboarding_verify_mail/onboarding_verify_mail_widget.dart
@@ -1,1 +1,144 @@
-sads
+
+class OnboardingVerifyMailWidget extends StatefulWidget {
+  const OnboardingVerifyMailWidget({super.key});
+
+  @override
+  State<OnboardingVerifyMailWidget> createState() =>
+      _OnboardingVerifyMailWidgetState();
+}
+
+class _OnboardingVerifyMailWidgetState extends State<OnboardingVerifyMailWidget> {
+  late OnboardingVerifyMailModel _model;
+
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => OnboardingVerifyMailModel());
+
+    logFirebaseEvent('screen_view', parameters: {'screen_name': 'Onboarding_VerifyMail'});
+    
+    WidgetsBinding.instance.addPostFrameCallback((_) => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    _model.dispose();
+
+    super.dispose();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => _model.unfocusNode.canRequestFocus
+          ? FocusScope.of(context).requestFocus(_model.unfocusNode)
+          : FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        key: scaffoldKey,
+        backgroundColor: CustomFlowTheme.of(context).primaryBackground,
+        body: SafeArea(
+          top: true,
+          child: Align(
+            alignment: const AlignmentDirectional(0, 0),
+            child: Padding(
+              padding: const EdgeInsets.all(24),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ////////////////
+                  //CUSTOM BAR
+                  /////////////////
+                  wrapWithModel(
+                    model: _model.customAppbarModel,
+                    updateCallback: () => setState(() {}),
+                    child: CustomAppbarWidget(
+                      backButton: true,
+                      actionButton: false,
+                      actionButtonAction: () async {},
+                      optionsButtonAction: () async {},
+                    ),
+                  ),
+                  ////////////////
+                  //PAGE TITLE
+                  /////////////////
+                  Padding(
+                    padding: const EdgeInsetsDirectional.fromSTEB(0, 24, 0, 0),
+                    child: Text(
+                      'Registrati',
+                      style: CustomFlowTheme.of(context).displaySmall,
+                    ),
+                  ),
+                  ////////////////
+                  //VALIDATION BUTTON
+                  /////////////////
+                  Padding(
+                    padding: const EdgeInsetsDirectional.fromSTEB(0, 24, 0, 0),
+                    child: AFButtonWidget(
+                      onPressed: () async {
+                        logFirebaseEvent('ONBOARDING_CREATE_ACCOUNT_CREATE_ACCOUNT');
+                        logFirebaseEvent('Button_validate_form');
+                        if (_model.formKey.currentState == null ||
+                            !_model.formKey.currentState!.validate()) {
+                          return;
+                        }
+                        logFirebaseEvent('Button_haptic_feedback');
+                        HapticFeedback.lightImpact();
+                        logFirebaseEvent('Button_auth');
+                        GoRouter.of(context).prepareAuthEvent();
+
+                        //REGISTRATION
+                        final user = await authManager.createAccountWithEmail(
+                          context,
+                          _model.emailAddressTextController.text,
+                          _model.passwordTextController.text,
+                        );
+                        if (user == null) {
+                          return;
+                        }
+                        //REGISTRATION
+                        await UsersRecord.collection.doc(user.uid).update({
+                          ...createUsersRecordData(
+                            displayName: _model.fullNameTextController.text,
+                            createdTime: DateTime.now(),
+                          ),
+                          /*
+                          ...mapToFirestore(
+                            {
+                            },
+                          ),*/
+                        });
+
+                        logFirebaseEvent('Button_navigate_to');
+                        context.goNamedAuth('Dashboard', context.mounted);
+                      },
+                      text: 'Crea Account',
+                      options: AFButtonOptions(
+                        width: double.infinity,
+                        height: 50,
+                        padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+                        iconPadding: const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+                        color: CustomFlowTheme.of(context).primary,
+                        textStyle: CustomFlowTheme.of(context).titleSmall,
+                        elevation: 0,
+                        borderSide: const BorderSide(
+                          color: Colors.transparent,
+                          width: 1,
+                        ),
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
