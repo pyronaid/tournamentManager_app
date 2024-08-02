beb320d789c1e38ad54729c9a3c2486e36cce075
pyronaid <64960586+pyronaid@users.noreply.github.com>
2024-08-02 12:42:54 +0200

    Update onboarding_verify_mail_model.dart

--- lib/pages/onboarding/onboarding_verify_mail/onboarding_verify_mail_model.dart
+++ lib/pages/onboarding/onboarding_verify_mail/onboarding_verify_mail_model.dart
@@ -1,1 +1,19 @@
-asdad
+class OnboardingVerifyMailModel extends CustomFlowModel<OnboardingVerifyMailWidget> {
+  ///  State fields for stateful widgets in this page.
+
+  final unfocusNode = FocusNode();
+  // Model for customAppbar component.
+  late CustomAppbarModel customAppbarModel;
+  
+
+  @override
+  void initState(BuildContext context) {
+    customAppbarModel = createModel(context, () => CustomAppbarModel());
+  }
+
+  @override
+  void dispose() {
+    unfocusNode.dispose();
+    customAppbarModel.dispose();
+  }
+}
