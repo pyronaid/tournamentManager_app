8f14bac180a8e6fdcb241b77be2f8f62d6261921
pyronaid <64960586+pyronaid@users.noreply.github.com>
2024-08-02 11:07:47 +0200

    Update forgot_password_widget.dart

--- lib/pages/onboarding/forgot_password/forgot_password_widget.dart
+++ lib/pages/onboarding/forgot_password/forgot_password_widget.dart
@@ -141,49 +141,7 @@ class _ForgotPasswordWidgetState extends State<ForgotPasswordWidget> {
                                         textCapitalization: TextCapitalization.none,
                                         textInputAction: TextInputAction.next,
                                         obscureText: false,
-                                        decoration: InputDecoration(
-                                          enabledBorder: OutlineInputBorder(
-                                            borderSide: BorderSide(
-                                              color: CustomFlowTheme
-                                                  .of(context)
-                                                  .alternate,
-                                              width: 1,
-                                            ),
-                                            borderRadius: BorderRadius.circular(8),
-                                          ),
-                                          focusedBorder: OutlineInputBorder(
-                                            borderSide: BorderSide(
-                                              color: CustomFlowTheme
-                                                  .of(context)
-                                                  .primary,
-                                              width: 1,
-                                            ),
-                                            borderRadius: BorderRadius.circular(8),
-                                          ),
-                                          errorBorder: OutlineInputBorder(
-                                            borderSide: BorderSide(
-                                              color: CustomFlowTheme
-                                                  .of(context)
-                                                  .error,
-                                              width: 1,
-                                            ),
-                                            borderRadius: BorderRadius.circular(8),
-                                          ),
-                                          focusedErrorBorder: OutlineInputBorder(
-                                            borderSide: BorderSide(
-                                              color: CustomFlowTheme
-                                                  .of(context)
-                                                  .error,
-                                              width: 1,
-                                            ),
-                                            borderRadius: BorderRadius.circular(8),
-                                          ),
-                                          filled: true,
-                                          fillColor: CustomFlowTheme
-                                              .of(context)
-                                              .secondaryBackground,
-                                          errorMaxLines: 2,
-                                        ),
+                                        decoration: standardInputDecoration(context),
                                         style: CustomFlowTheme
                                             .of(context)
                                             .bodyMedium
